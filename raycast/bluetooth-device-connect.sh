f49f43f1f7cff2f16a79afce2ee2be5166cdf1f4
Maniac <52683414+Maniacsan@users.noreply.github.com>
2025-02-17 06:08:42 -0800

    Update bluetooth-device-connect.sh

--- raycast/bluetooth-device-connect.sh
+++ raycast/bluetooth-device-connect.sh
@@ -13,6 +13,16 @@
 # @raycast.description Ensures bluetooth is one and connects to a bluetooth device
 # @raycast.author Maniac
 
-blueutil --power 1 && blueutil --connect 00-c5-85-88-02-9f
 
+# 1. Connect to your bluetooth manually
+# 2. Enter the following in terminal to find
+#    Mac address of your connected device:
+#    blueutil --paired
+# 3. Paste mac address to the following variable
+DEVICE_MAC_ADDRESS="XX:XX:XX:XX:XX:XX"
+
+blueutil --power 1 # Makes sure bluetooth is on
+blueutil --connect $DEVICE_MAC_ADDRESS # Connects to bluetooth device
+
+sleep 2
 echo "Bluetooth Device Connected"
